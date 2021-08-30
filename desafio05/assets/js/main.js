52f689e3ea0d03c0923cafe3ee45fc14f8febda4
enzosarmento <enzosarmento91@gmail.com>
2021-08-30 18:31:16 -0300

    feat: desafio05

---
+++ desafio05/assets/js/main.js
@@ -0,0 +1,26 @@
+const app = new Vue({
+    el: '#app',
+    data: {
+        socials: [
+            {
+                link: 'https://facebook.com/FansOfStudioGhibli/', 
+                src:'assets/images/facebook.svg'
+            },
+            {
+                link: 'https://www.instagram.com/ghibliarchives/', 
+                src:'assets/images/instagram.svg'
+            },
+            {
+                link: 'https://twitter.com/JP_GHIBLI', 
+                src:'assets/images/twitter.svg'
+            },
+            {
+                link: 'https://youtube.com', 
+                src:'assets/images/youtube.svg'
+            }
+        ]
+    },
+    methods: {
+
+    }
+})
